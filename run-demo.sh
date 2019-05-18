b3c03b62d2c043babab82e99552318d5a21fcbe3
Sébastien Cevey <seb@cine7.net>
2019-05-18 07:30:22 +0100

    Add missing run-demo.sh

---
+++ run-demo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd scenes
+../src/zarbee -f -s ../resources/14\ -\ Comic\ Bakery.ogg \
+    intro.xml \
+    tunnel-1.xml \
+    city.xml \
+    tunnel-2.xml \
+    sea.xml \
+    tunnel-3.xml \
+    robot.xml \
+    tunnel-4.xml \
+    psy-bee.xml
+
